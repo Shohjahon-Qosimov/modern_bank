3bb59b4614eca515b6a92c6135db7b88be574dbe
Shohjahon-Qosimov <132648858+Shohjahon-Qosimov@users.noreply.github.com>
2024-01-10 00:34:32 +0500

    initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = ({ styles }) => (
+  <button
+    type="button"
+    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+    onClick={() => {
+      window.open("https://t.me/shohjahonqosimov");
+    }}
+  >
+    Get Started
+  </button>
+);
+
+export default Button;
